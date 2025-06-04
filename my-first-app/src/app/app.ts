fcf7e1cc6b768ec8e0a70a397c70be7357909211
shiva <sreddy@hey.com>
2025-06-04 07:45:26 -0400

    .

--- my-first-app/src/app/app.ts
+++ my-first-app/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+@Component({
+  selector: 'app-user',
+  template: `
+    Username: {{ username }}
+  `,
+})
+export class User {
+  username = 'youngTech';
+}
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'my-first-app';
+  city = 'San Fransisco';
 }
